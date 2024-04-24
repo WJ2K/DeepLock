e814aaac7fa1668161e0a4a8c1e25f502b933dd3
Joe Stella <157044883+JoeStellaTU@users.noreply.github.com>
2024-04-24 09:14:06 -0400

    update!

--- app/src/main/java/com/example/deeplock/MainActivity.java
+++ app/src/main/java/com/example/deeplock/MainActivity.java
@@ -2,39 +2,45 @@ package com.example.deeplock;
 
 import android.app.Activity;
 import android.content.Context;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 
 
-public class MainActivity extends Activity  {
+public class MainActivity extends AppCompatActivity {
 
     SensorListener sensorListener;
     Context context = this;
-    MediaPlayer mp[] = new MediaPlayer[12];
 
     int pocket = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
         sensorListener = new SensorListener(this, context);
-
-        prepare();
 
     }
 
-    public void detect(float prox, float light, float g[], int inc){
-        if((prox<1)&&(light<2)&&(g[1]<-0.6)&&( (inc>75)||(inc<100))){
-            pocket=1;
+    public void detect(float prox, float light, float[] g, int inc) {
+        if ((prox < 1) && (light < 2) && (g[1] < -0.6) && ((inc > 75) || (inc < 100))) {
+            pocket = 1;
         }
-        if((prox>=1)&&(light>=2)&&(g[1]>=-0.7)){
-            if(pocket==1){
-                playSound();
-                pocket=0;
+        if ((prox >= 1) && (light >= 2) && (g[1] >= -0.7)) {
+            if (pocket == 1) {
+
+                pocket = 0;
             }
         }
     }
+
+    //public void fullLockdown(){
+
+    //}
+
+}
